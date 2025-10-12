2d6df1fd4ea40852c3ef6e6ca6a39b9b4a20174e
rafisaiyari <rafisaiyari@ymail.com>
2025-10-12 23:43:24 +0800

    mobile footer fixed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,13 @@
 import { mySocials } from "../constants"
+import { useMediaQuery } from "react-responsive"
+
 const Footer = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853 })
+
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <section className={`flex flex-wrap gap-5 pb-3 text-sm text-neutral-400 c-space ${
+      isMobile ? "flex-col items-center justify-center" : "items-center justify-between"
+    }`}>
         <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full"/>
         <div className="flex gap-2">
             <p>Terms & Conditions</p>
